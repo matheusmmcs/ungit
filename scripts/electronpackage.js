6fbf7ac057e27e56a18bb97f5e9d1f577c19a7f6
Matheus <matheusmmcs@gmail.com>
2020-06-02 22:04:31 -0300

    Criação do app para windows, linux e mac no mesmo comando

--- scripts/electronpackage.js
+++ scripts/electronpackage.js
@@ -4,11 +4,23 @@ const electronPackager = require('electron-packager');
 
 const baseDir = path.join(__dirname, '..');
 
+//include parametrized build
+const args = process.argv.slice(2);
+const re = /\-\-([\w|\d]*)\=([\w|\d]*)/;
+let obj = {};
+args.forEach(e => {
+    const ar = e.match(re);
+    obj[ar[1]] = ar[2];
+})
+
+console.log('PARAMS: ', obj);
+
 electronPackager({
+  ...obj,
   dir: baseDir,
   out: path.join(baseDir, 'build'),
   icon: path.join(baseDir, 'public/images/icon'),
-  all: process.argv.includes('--all'),
+  //all: process.argv.includes('--all'),
   asar: true,
   overwrite: true,
   ignore: [
